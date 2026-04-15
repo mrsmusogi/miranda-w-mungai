2ed8ad1d85c3a7b3e25d9cc24fd93ceb67d84530
joe mac <jhmccarney@gmail.com>
2026-04-15 13:11:39 +0100

    Basic template implemented #TODO vault-cms

---
+++ src/assets/content.js
@@ -0,0 +1,104 @@
+
+
+
+export const categories =[
+    { id: 1, name: "comics" },
+    { id: 2, name: "logs" },
+    { id: 3, name: "experiments" },
+    { id: 4, name: "film work" },
+    { id: 5, name: "screenings & discussions" },
+    { id: 6, name: "about" },
+  ]
+export const posts = [
+    {
+      id: 1,
+      category: "film work",
+      title: "documentary programmer",
+      body: "to be continued...",
+      published: true,
+    },
+    {
+      id: 2,
+      category: "film work",
+      title: "industry events producer",
+      body: " *'Performative Practice on Film'* Panel Discussion @ London Short Film Festival 2020 <br> ↪ w/ Onyeka Igwe, Maryam Tafakory and Lanre Malaolu <br> *'Politicising Form: Artist filmmakers in Conversation'* @ London Short Film Festival 2020 <br> ↪ w/ Morgan Quaintance, Andrea Luka Zimmerman and Kamal Aljafari <br> *'The Ethics of Representation in Documentary'* @ London Short Film Festival 2019 <br>",
+      published: true,
+    },
+    {
+      id: 3,
+      category: "screenings & discussions",
+      title: "even",
+      body: "I worked with three other people to produce the Even launch event in October 2022. Our intention was to find a permanent space for experimental perfomance, music and film. For my part, I was invested in bringing artist moving image into non-normative exhibition spaces and experimenting with different contexts for open discussions. The project was a valuable learning experience, understanding first-hand the graft that goes into starting a DIY project from scratch with no fixed venue and no funds (bar £500 from the council to provide food and refreshments for artists) and realising that the community in Norwich was equally dedicated to the project (multiple sold out events!). The group doesn't exist anymore and I no longer live in Norwich, but since this effort two independent venues have been set up.",
+      published: false,
+    },
+    {
+      id: 4,
+      category: "screenings & discussions",
+      title: "entangled networks",
+      body: "I was selected from an open call to deliver three screening & discussion events responding to the theme of 'Reclaiming Digital Agency' for IMPAKT Festival 2023, Our Terms, Our Conditions <br/> to be continued...",
+      published: true,
+    },
+    {
+      id: 5,
+      category: "screenings & discussions",
+      title: "learning to imagine a future",
+      body: "Black histories, decolonial histories, histories at the intersection of class, race, and gender all offer an orientation towards futures of freedom, liberation, and solidarity. The power of these histories relies on recognising their enduring possibility, on bringing the past into conversation with the present, for the benefit of a future that feels increasingly unimaginable. <br/> OUTPOST becomes a generative space for discussion and the creative imagining of these futures, anchored in the images and words that escape the embalmment of time.<br/> to be continued...",
+      published: true,
+    },
+    {
+      id: 6,
+      category: "screenings & discussions",
+      title: "new cinema forum",
+      body: "The first edition of Berwick Film & Media Arts Festival’s New Cinema Forum is an invitation to artists, filmmakers, critics and arts/film workers to consider new orientations to creating, exhibiting and working collectively. Offering practical ways to sustain and develop your practice and community, the NCF is a relaxed space to reflect and make connections ahead of our Opening Night film.  I moderated the opnening conversation between participants and attendees considering the contexts we are all working in. Join a conversation considering the challenges, contradictions, and persistent sites of possibility that emerge. Anchoring the day with a shared framework, we will discuss support mechanisms, alternative structures, and new orientations to take in light of ever-changing circumstances. to be continued...",
+      published: false,
+    },
+    {
+      id: 7,
+      category: "experiments",
+      title: "montage experiments",
+      body: "I have been experimenting with montage since studying “A Dialectic Approach to Film Form” by Sergei Eisenstein during my undergraduate degree. I use footage from video games, youtube and The Internet Archive. I also use recordings I’ve shot myself using my smartphone or 2011 camcorder. I make soundtracks using Bosca Ceoil: The Blue Album. I have used default filename to find footage, and youtube-dl to download clips.",
+      published: true,
+    },
+    {
+      id: 8,
+      category: "screenings & discussions",
+      title: "ruins full of futures",
+      body: "Ruins Full of Futures features five films that differently represent the relationship between place-making, collective desire, and the ideas that shape the spaces we live, work and socialise in. Three filmmakers from the programme join an open conversation after the screening, where we discuss our experience of social, cultural and commercial spaces: the impressions they impart on us, the futures they foreclose on and the futures they make seem possible. I made a poster for the event, which is derivative of Yokoo Tadanori's poster for the 1966 publication The City and Design, The Wonders of Life on Earth by Isamu Kurita. I discovered this poster in an essay by Ryan Holmberg at the end of a deranged comic book called 'The Pits of Hell' by Ebisu Yoshikazu. I wrote an essay about the films.",
+      published: true,
+    },
+    {
+      id: 9,
+      category: "experiments",
+      title: "montage experiment 1",
+      body: "A short film about the myth of meritocracy.",
+      published: true,
+    },
+    {
+      id: 10,
+      category: "experiments",
+      title: "montage experiment 2",
+      body: "A short film about cinema.",
+      published: true,
+    },
+    {
+      id: 11,
+      category: "experiments",
+      title: "pollution flows downstream (montage experiment 3)",
+      body: "I made a short film about the regeneration of Elephant and Castle as part of the Radical Film School.",
+      published: true,
+    },
+    {
+      id: 12,
+      category: "experiments",
+      title: "montage experiment 4",
+      body: "I’m thinking about how to communicate meaning without using any spoken or written language. The meaning I’m trying to communicate is formal, these are all images that respond to the brief I gave myself of “circles and lines”. Video game footage is from Grand Theft Auto San Andreas, using the [Earth as Moon mod.](https://www.gtainside.com/en/sanandreas/mods/117412-earth-as-moon/)",
+      published: true,
+    },
+    {
+      id: 13,
+      category: "about",
+      title: "about",
+      body: "I am a freelance documentary film curator, events producer, and facilitator experimenting with post-screening discussion. I source films, and facilitate discussions in response to the themes and ideas that emerge from the films. By mediating the world, film acts a starting point for discussion, offering space to reflect on and discuss contemporary and historical issues. The intention is simply to exercise our capacity for collective enquiry. The open-discussion format invites everyone to participate in a conversation at a time of great division, social atomisation and loneliness. The themes of discussions respond to a contemporary issue or struggle. Each concern tends to be representative of larger, more abstract issues. Usually they point to some factor of limitation to the conditions of possibility. The aim is to interrogate this limit-point and find contractions in seemingly steadfast systems, institutions and practices that make our world, and our life in it. If you are looking for a facilitator for a post-screening dicussion, please [get in touch.](maito:mirandamungai@gmail.com)",
+      published: true,
+    },
+  ]
